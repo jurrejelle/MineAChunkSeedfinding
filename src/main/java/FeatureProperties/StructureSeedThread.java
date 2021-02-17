a2a2bd75e48bf701a5caba849f90393b3c7d2c24
Jurre Groenendijk <jurre@jilles.com>
2021-02-17 21:07:13 +0100

    Updated the main thread class

--- src/main/java/FeatureProperties/StructureSeedThread.java
+++ src/main/java/FeatureProperties/StructureSeedThread.java
@@ -39,7 +39,7 @@ public class StructureSeedThread implements Runnable{
     }
     public void run(){
         System.out.println("Started StructureSeedThread "+(offset+1)+"/"+totalThreads+" on seed nr: "+(start+offset));
-        ChunkRand myCR = new ChunkRand();
+        ChunkRand chunkRand = new ChunkRand();
 
         ArrayList<Long> structureSeeds = new ArrayList<>();
         // brute force through every structure seed
@@ -48,55 +48,39 @@ public class StructureSeedThread implements Runnable{
             if(structureSeed%1000000000==0){
                 System.out.println(structureSeed+"L");
             }
-            int ravineCounter=0;
-            int[][] ravines = new int[10][2];
+            ArrayList<RavineProperties> ravines = new ArrayList<>();
             //Check for ravines in the chunks arond spawn (so x=[-1,0,1] z=[-1,0,1])
             for(int x=-1;x<=1;x++){
                 for(int z=-1;z<=1;z++) {
                     //Spawn a ravineProperties class at our coordinates
-                    RavineProperties rp = new RavineProperties(structureSeed, new CPos(x, z));
-                    Float myWidth = rp.getWidth(myCR);
+                    RavineProperties ravine = new RavineProperties(structureSeed, new CPos(x, z));
                     //Skip anything that either isn't a ravine or is less than 5.6 wide (max width: 6.0)
-                    if (myWidth == null || myWidth <5.6) continue;
-                    //Add the x,z chunk coordinates to a list, and increase the number of ravines found (i) by 1
-                    ravines[ravineCounter][0] = x;
-                    ravines[ravineCounter][1] = z;
-                    ravineCounter++;
+                    if(!ravine.generate(chunkRand)) continue;
+                    if(ravine.width <5.6) continue;
+                    if(ravine.blockPosition.getY()<35) continue;
+                    //Add the ravine to a list, and increase the number of ravines found (ravineCounter) by 1
+                    ravines.add(ravine);
                 }
             }
             //If there are 2 or more ravines of our width
-            if(ravineCounter>1) {
-                ArrayList<Float[]> yaws = new ArrayList<>();
-                //Go through all the ravines and add the x, z of their starts and the yaw (angle in radians) to an array
-                for (int j = 0; j < ravineCounter; j++) {
-                    RavineProperties rp = new RavineProperties(structureSeed, new CPos(ravines[j][0], ravines[j][1]));
-                    BPos position = rp.getPosition(myCR);
-                    //Skip if the ravine is too low because of high chance of lava
-                    if(position.getY()<35){continue;}
-
-                    Float x = Integer.valueOf(position.getX()).floatValue();
-                    Float z = Integer.valueOf(position.getZ()).floatValue();
-                    //Add the info we need for the intersection check to an array
-                    yaws.add(new Float[]{rp.getYaw(myCR), x, z});
-                }
-                //If there are less than 2 left we can discard this structure seed and try the next one
-                if(yaws.size()<2) {
-                    continue;
-                }
-                for (int yaw1index=0;yaw1index<yaws.size();yaw1index++) {
-                    for (int yaw2index=yaw1index+1;yaw2index<yaws.size();yaw2index++) {
-                        Float[] yaw1 = yaws.get(yaw1index);
-                        Float[] yaw2 = yaws.get(yaw2index);
-                        AtomicInteger x = new AtomicInteger(-1000);
-                        AtomicInteger z = new AtomicInteger(-1000);
+            int ravineCounter = ravines.size();
+            if(ravineCounter >=2) {
+                for (int i=0;i<ravineCounter-1;i++) {
+                    for (int j=i+1;j<ravineCounter;j++) {
+                        RavineProperties ravine1 = ravines.get(i);
+                        RavineProperties ravine2 = ravines.get(j);
+                        AtomicInteger intersectX = new AtomicInteger(-1000);
+                        AtomicInteger intersectZ = new AtomicInteger(-1000);
                         //Check if both ravines intersect and if they are relatively close to being parralel
-                        if (intersect(yaw1[1], yaw1[2], yaw2[1], yaw2[2], yaw1[0], yaw2[0], 35, x, z) &&
-                                Math.abs(yaw1[0]-yaw2[0])<0.5) {
+                        if (intersect(ravine1.blockPosition.getX(), ravine1.blockPosition.getZ(),
+                                ravine2.blockPosition.getX(), ravine2.blockPosition.getZ(),
+                                ravine1.yaw, ravine2.yaw, 50, intersectX, intersectZ) &&
+                                Math.abs(ravine1.yaw-ravine2.yaw)<0.5) {
                             //Make sure we print only once for every structure seed
                             if (!structureSeeds.contains(structureSeed)) {
                                 //Print the structure seed and intersection coordinates
                                 structureSeeds.add(structureSeed);
-                                System.out.println("{" + structureSeed + "L," + x.get() + "," + z.get() +"},");
+                                System.out.println("{" + structureSeed + "L," + intersectX.get() + "," + intersectZ.get() +"},");
                             }
                         }
                     }
